fd05c16a66c2eca32981a20eec74c315c3e3896d
Eric McGowan <ericw.mcgowan@gmail.com>
2019-06-06 13:43:29 -0500

    First Commit

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+    "name": "Clayton Edward",
+    "photo": "",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Tom Petty",
+    "photo": "",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    "name": "Clint Eastwood",
+    "photo": "",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    "name": "Pauly Shore",
+    "photo": "",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    "name": "Myles Kennedy",
+    "photo": "",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+    "name": "Mark Tremonti",
+    "photo": "",
+    "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+},
+{
+    "name": "Mickey Mouse",
+    "photo": "",
+    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
+
+]
+
+module.exports = friends;
